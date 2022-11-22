b3dfaea681cdd194d9f179d27e9a2b1f4fb6c27a
Parmveer Baweja <98068792+parmv33r@users.noreply.github.com>
2022-11-21 20:30:40 -0700

    added list to list sample

--- ChinookMusic/ChinookSystem/ChinookExtensions.cs
+++ ChinookMusic/ChinookSystem/ChinookExtensions.cs
@@ -56,6 +56,14 @@ namespace ChinookSystem
                 var context = serviceProvider.GetRequiredService<ChinookContext>();
                 return new PlaylistTrackServices(context);
             });
+
+            services.AddTransient<AboutServices>((serviceProvider) =>
+            {
+                //retrieve the registered DcContext done with
+                //  AddDbContext
+                var context = serviceProvider.GetRequiredService<ChinookContext>();
+                return new AboutServices(context);
+            });
         }
     }
 }
